41509b23dd3592b4da9f5714410084855264f97c
Muminah <105560838+mumi-nah@users.noreply.github.com>
2026-07-27 22:14:31 +0100

    Remove comments from data.tf
    Removed comments explaining the data source requirement.

--- data.tf
+++ data.tf
@@ -1,6 +1,3 @@
-# Data sources requirement: this reads a file that already exists on disk
-# (committed with the repo) rather than one Terraform manages/creates.
-# It's a good example of "pull in existing information" vs "create a resource".
 data "local_file" "app_template" {
   filename = "${path.module}/templates/app.conf.tpl"
 }
